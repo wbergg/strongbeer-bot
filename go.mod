9d8189e6b3853fd35977f9afc4bf9b85fc0ae218
wberg <wberg@wberg.com>
2024-04-15 13:37:00 +0200

    updates

--- go.mod
+++ go.mod
@@ -1,10 +1,15 @@
-module github.com/strongbeer-bot
+module github.com/wbergg/strongbeer-bot
 
 go 1.22.1
 
-require github.com/wbergg/bordershop-bot v0.0.0-20210408112644-cef54d8f310d
+require (
+	github.com/sirupsen/logrus v1.9.3
+	github.com/wbergg/bordershop-bot v0.0.0-20210408112644-cef54d8f310d
+)
 
 require (
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
 )
--- main.go
+++ main.go
@@ -3,32 +3,73 @@ package main
 import (
 	"flag"
 	"fmt"
+	"time"
 
 	log "github.com/sirupsen/logrus"
 
 	"github.com/wbergg/strongbeer-bot/creds"
-
-	"github.com/wbergg/bordershop-bot/tele"
 )
+
+var announce bool = false
 
 func main() {
 	// Enable bool debug flag
-	debug := flag.Bool("debug", false, "Turns on debug mode and prints to stdout")
+	debug := flag.Bool("debug", false, "true/false - Turns on debug mode and prints to stdout")
+	flag.Parse()
 
 	// Load credentials
-	credentials, err := creds.LoadSecrets()
+	credentials, err := creds.LoadCreds()
 	if err != nil {
 		log.Error(err)
 		panic("Could not load credentials, check creds/creds.json")
 	}
-
-	tg := tele.New(tgAPIKey, tgChannel, false, *debug)
-	tg.Init(false)
+	fmt.Println(credentials.Telegram)
+	//tg := tele.New(credentials.Telegram.tgChannel, credentials.Telegram.tgAPIKey, false, *debug)
+	//tg.Init(false)
 
 	// Run before starting the timer
-	mondayTimer(tg)
+	//mondayTimer(tg)
+	for {
+		if *debug {
+			fmt.Println("true")
+		} else {
+			mondayTimer()
+		}
+		time.Sleep(5 * time.Second)
+	}
+
 }
 
-func mondayTimer(t *tele.Tele) {
+// func mondayTimer(t *tele.Tele) {
+func mondayTimer() {
+
+	t := time.Now()
+
+	if t.Weekday() == time.Monday {
+		if announce == false {
+			fmt.Println("Monday")
+			announce = true
+			go mondayReminer(12)
+			go mondayReminer(18)
+		} else {
+			return
+		}
+		if t.Weekday() != time.Monday {
+			announce = false
+		}
+	}
+
 	fmt.Println("test")
+}
+
+func mondayReminer(d time.Duration) {
+	timer := time.NewTimer(d * time.Second)
+
+	<-timer.C
+	fmt.Println("Timer fired!")
+
+}
+
+func readMessage() {
+
 }
